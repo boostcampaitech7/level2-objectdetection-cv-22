ef009a36f36279bcc25583a998ed01f1f0804408
mongsam2 <songjaehyun121476@gmail.com>
2024-10-16 11:40:15 +0900

    [feat] wandb logger 추가

--- stratified_group_kfold/custom_trainer.py
+++ stratified_group_kfold/custom_trainer.py
@@ -1,12 +1,15 @@
 import os
 import copy
 import torch
+import wandb
 
 import detectron2.data.transforms as T
 from detectron2.data import detection_utils as utils
 from detectron2.engine import DefaultTrainer
 from detectron2.data import build_detection_train_loader
 from detectron2.evaluation import COCOEvaluator
+from detectron2.utils.events import EventWriter
+from detectron2.engine import hooks
 
 def MyMapper(dataset_dict):
     dataset_dict = copy.deepcopy(dataset_dict)
@@ -52,4 +55,35 @@ class MyTrainer(DefaultTrainer):
             output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
             os.makedirs(output_folder, exist_ok=True)
 
+    def build_hooks(self):
+        hooks_list = super().build_hooks()
+        # WandbLogger의 trainer 설정
+        wandb_logger = WandbLogger()
+        wandb_logger.set_trainer(self)
+        hooks_list.append(hooks.PeriodicWriter([wandb_logger], period=100))
+        return hooks_list
+
         return COCOEvaluator(dataset_name, output_dir=output_folder)
+
+# wandb logger
+class WandbLogger(EventWriter):
+    def __init__(self):
+        super().__init__()
+        self.trainer = None  # trainer를 저장할 변수 초기화
+
+    def write(self):
+        # 학습 중 발생하는 메트릭을 Wandb에 기록
+        if self.trainer is not None:
+            # 모든 메트릭을 가져와서 Wandb에 로그
+            metrics = {k: v.median(20) for k, v in self.trainer.storage.histories().items()}
+            wandb.log(metrics)
+
+    def set_trainer(self, trainer):
+        # trainer 객체 설정
+        self.trainer = trainer
+
+    def after_step(self):
+        self.write()
+
+    def after_train(self):
+        self.write()
